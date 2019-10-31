637ffba098bf05754d91d69c8e551f803ee5c039
Serena Gibbons <serenalgibbons@gmail.com>
2019-10-31 15:38:11 -0400

    Initial commit

---
+++ Lab-GridMasterDetail/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_GridMasterDetail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string filename = "MovieDataFile.txt";
+
+            StreamReader sr = new StreamReader(filename);
+            while (!sr.EndOfStream)
+            {
+                // create movie instance
+                Movie movie = new Movie();
+
+                movie.Name = sr.ReadLine();
+                movie.RottenTomatoScore = sr.ReadLine();
+                movie.Review = sr.ReadLine();
+               
+                // add movie to listbox
+                listBoxMovie.Items.Add(movie);
+            }
+            sr.Close();
+        }
+
+        private void ListBoxMovie_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // get the item selected
+            IList items = (IList) e.AddedItems;
+
+            // cast as a movie object
+            Movie m = (Movie) items[0];
+
+            // set text fields
+            txtName.Text = m.Name;
+            txtRTS.Text = m.RottenTomatoScore;
+            txtReview.Text = m.Review;
+        }
+    }
+}
---
+++ Lab-GridMasterDetail/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_GridMasterDetail
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string RottenTomatoScore { get; set; }
+        public string Review { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
